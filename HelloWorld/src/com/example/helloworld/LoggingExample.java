b58eda6d94d1a3b9527f13823e8f2dd7132170a5
Brian Woestman <woestmab@gmail.com>
2016-01-31 10:36:42 -0600

    initial commit

---
+++ HelloWorld/src/com/example/helloworld/LoggingExample.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import org.apache.log4j.*;
+
+/**
+ * Created by Bdub on 1/28/16.
+ */
+
+public class LoggingExample {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    public LoggingExample() {
+        log.setLevel(Level.WARN);
+
+        log.trace("Trace Message!");
+        log.debug("Debug Message!");
+        log.info("Info Message!");
+        log.warn("Warn Message!");
+        log.error("Error Message!");
+        log.fatal("Fatal Message!");
+    }
+}
